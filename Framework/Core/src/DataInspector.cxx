9a039a35cd6c0a54b51f218f4b190ebc75b08012
mrzysztof <krzysztof.maliszewski9@wp.pl>
2022-09-03 20:04:00 +0200

    sendToProxy refactor and fix arrow

--- Framework/Core/src/DataInspector.cxx
+++ Framework/Core/src/DataInspector.cxx
@@ -28,6 +28,8 @@
 #include <boost/algorithm/string/join.hpp>
 #include "Framework/DISocket.hpp"
 #include "Framework/DataInspectorService.h"
+#include <arrow/table.h>
+#include "Framework/TableConsumer.h"
 
 using namespace rapidjson;
 
@@ -94,6 +96,65 @@ namespace o2::framework
     return "";
   }
 
+  void addPayload(Document& message,
+                  const header::DataHeader* header,
+                  const DataRef& ref,
+                  Document::AllocatorType& alloc){
+    if (header->payloadSerializationMethod == header::gSerializationMethodROOT) {
+      std::unique_ptr<TObject> object = DataRefUtils::as<TObject>(ref);
+      TString json = TBufferJSON::ToJSON(object.get());
+      message.AddMember("payload", Value(json.Data(), alloc), alloc);
+    }
+    else if(header->payloadSerializationMethod == header::gSerializationMethodArrow) {
+        TableConsumer consumer = TableConsumer(reinterpret_cast<const uint8_t*>(ref.payload), header->payloadSize);
+        auto table = consumer.asArrowTable();
+        message.AddMember("payload", Value(table->ToString().c_str(), alloc), alloc);
+    }
+  }
+
+  void addBasicHeaderInfo(Document& message, const header::DataHeader* header, Document::AllocatorType& alloc){
+    std::string origin = header->dataOrigin.as<std::string>();
+    std::string description = header->dataDescription.as<std::string>();
+    std::string method = header->payloadSerializationMethod.as<std::string>();
+
+    message.AddMember("origin", Value(origin.c_str(), alloc), alloc);
+    message.AddMember("description", Value(description.c_str(), alloc), alloc);
+    message.AddMember("subSpecification", Value(header->subSpecification), alloc);
+    message.AddMember("firstTForbit", Value(header->firstTForbit), alloc);
+    message.AddMember("tfCounter", Value(header->tfCounter), alloc);
+    message.AddMember("runNumber", Value(header->runNumber), alloc);
+    message.AddMember("payloadSize", Value(header->payloadSize), alloc);
+    message.AddMember("splitPayloadParts", Value(header->splitPayloadParts), alloc);
+    message.AddMember("payloadSerialization", Value(method.c_str(), alloc), alloc);
+    message.AddMember("payloadSplitIndex", Value(header->splitPayloadIndex), alloc);
+  }
+
+  void buildDocument(Document& message, std::string sender, const DataRef& ref){
+    message.SetObject();
+    Document::AllocatorType& alloc = message.GetAllocator();
+    message.AddMember("sender", Value(sender.c_str(), alloc), alloc);
+
+    const header::BaseHeader* baseHeader = header::BaseHeader::get(reinterpret_cast<const std::byte*>(ref.header));
+    for (; baseHeader != nullptr; baseHeader = baseHeader->next()) {
+      if (baseHeader->description == header::DataHeader::sHeaderType) {
+        const auto* header = header::get<header::DataHeader*>(baseHeader->data());
+        addBasicHeaderInfo(message, header, alloc);
+        addPayload(message, header, ref, alloc);
+      }
+      else if (baseHeader->description == DataProcessingHeader::sHeaderType) {
+        const auto* header = header::get<DataProcessingHeader*>(baseHeader->data());
+
+        message.AddMember("startTime", Value(header->startTime), alloc);
+        message.AddMember("duration", Value(header->duration), alloc);
+        message.AddMember("creationTimer", Value(header->creation), alloc);
+      }
+      else if (baseHeader->description == OutputObjHeader::sHeaderType) {
+        const auto* header = header::get<OutputObjHeader*>(baseHeader->data());
+        message.AddMember("taskHash", Value(header->mTaskHash), alloc);
+      }
+    }
+  }
+
   /* Callback which transforms each `DataRef` in `context` to a JSON object and
   sends it on the `socket`. The messages are sent separately. */
   static void sendToProxy(ProcessingContext& context)
@@ -102,53 +163,11 @@ namespace o2::framework
 
     DeviceSpec device = context.services().get<RawDeviceService>().spec();
     for (const DataRef& ref : context.inputs()) {
-      Document message;
-      message.SetObject();
-      Document::AllocatorType& alloc = message.GetAllocator();
-
       std::string sender = findSenderByRoute(device.inputs, *ref.spec);
-      message.AddMember("sender", Value(sender.c_str(), alloc), alloc);
-
-      const header::BaseHeader* baseHeader = header::BaseHeader::get(reinterpret_cast<const std::byte*>(ref.header));
-      for (; baseHeader != nullptr; baseHeader = baseHeader->next()) {
-        if (baseHeader->description == header::DataHeader::sHeaderType) {
-          const auto* header = header::get<header::DataHeader*>(baseHeader->data());
-          std::string origin = header->dataOrigin.as<std::string>();
-          std::string description = header->dataDescription.as<std::string>();
-          std::string method = header->payloadSerializationMethod.as<std::string>();
-
-          message.AddMember("origin", Value(origin.c_str(), alloc), alloc);
-          message.AddMember("description", Value(description.c_str(), alloc), alloc);
-          message.AddMember("subSpecification", Value(header->subSpecification), alloc);
-          message.AddMember("firstTForbit", Value(header->firstTForbit), alloc);
-          message.AddMember("tfCounter", Value(header->tfCounter), alloc);
-          message.AddMember("runNumber", Value(header->runNumber), alloc);
-          message.AddMember("payloadSize", Value(header->payloadSize), alloc);
-          message.AddMember("splitPayloadParts", Value(header->splitPayloadParts), alloc);
-          message.AddMember("payloadSerialization", Value(method.c_str(), alloc), alloc);
-          message.AddMember("payloadSplitIndex", Value(header->splitPayloadIndex), alloc);
-          if (header->payloadSerializationMethod == header::gSerializationMethodROOT) {
-            std::unique_ptr<TObject> object = DataRefUtils::as<TObject>(ref);
-            TString json = TBufferJSON::ToJSON(object.get());
-            message.AddMember("payload", Value(json.Data(), alloc), alloc);
-          }
-          else if(header->payloadSerializationMethod == header::gSerializationMethodArrow){
-            TableConsumer cons = TableConsumer(reinterpret_cast<const uint8_t* >(ref.payload), header->payloadSize);
-            auto table = cons.asArrowTable();
-            message.AddMember("payload", Value(table->ToString().c_str(), alloc), alloc);
-          }
-        } else if (baseHeader->description == DataProcessingHeader::sHeaderType) {
-          const auto* header = header::get<DataProcessingHeader*>(baseHeader->data());
-          message.AddMember("startTime", Value(header->startTime), alloc);
-          message.AddMember("duration", Value(header->duration), alloc);
-          message.AddMember("creationTimer", Value(header->creation), alloc);
-        } else if (baseHeader->description == OutputObjHeader::sHeaderType) {
-          const auto* header = header::get<OutputObjHeader*>(baseHeader->data());
-          message.AddMember("taskHash", Value(header->mTaskHash), alloc);
-        }
-      }
+      Document message;
       StringBuffer buffer;
       Writer<StringBuffer> writer(buffer);
+      buildDocument(message, sender, ref);
       message.Accept(writer);
 
       socket.send(DIMessage{DIMessage::Header::Type::DATA, std::string{buffer.GetString(), buffer.GetSize()}});
